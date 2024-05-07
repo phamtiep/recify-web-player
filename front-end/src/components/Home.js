d16d493ce99800dfbfcead258275f218f34cdc67
vinhhoanglong <longvinhvhbk@gmail.com>
2024-05-07 14:45:45 +0700

    update login and homepage

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -6,30 +6,30 @@ function Home() {
 <section class="pt-24 homepage">
     <div class="px-12 mx-auto max-w-7xl">
         <div class="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
-            <h1 class="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
+            <h1 class="mb-8 text-4xl font-extrabold leading-none tracking-normal text-white md:text-6xl md:tracking-tight">
                 <span>Start</span> <span> discover your favourite music </span>
             </h1>
-            <p class="px-0 mb-8 text-lg text-black md:text-xl lg:px-24">
-            Welcome to Recify - your ultimate destination for all things music! Immerse yourself in a world of endless melodies, rhythms, and beats right at your fingertips.
+            <p className="px-0 mb-8 text-lg text-white md:text-xl lg:px-24 text-animation">
+                 Welcome to Recify - your ultimate destination for all things music! 
             </p>
             <div class="mb-4 space-x-0 md:space-x-8 md:mb-8">
                 <NavLink to={"/player"}>
                     
-                    <a href="#_" class="inline-flex items-center justify-center w-full px-10 py-10 mb-2 text-xl text-white bg-black rounded-2xl sm:w-auto sm:mb-0">
+                    <a href="#_" class="inline-flex items-center justify-center w-full px-10 py-10 mb-2 text-xl text-white bg-gray-600 rounded-2xl sm:w-auto sm:mb-0">
                         Player
                     </a>
                     
                 </NavLink>
                 <NavLink to={"/player"}>
                     
-                    <a href="#_" class="inline-flex items-center justify-center w-full px-10 py-10 mb-2 text-xl text-white bg-black rounded-2xl sm:w-auto sm:mb-0">
+                    <a href="#_" class="inline-flex items-center justify-center w-full px-10 py-10 mb-2 text-xl text-white bg-gray-600 rounded-2xl sm:w-auto sm:mb-0">
                         Player
                     </a>
                     
                 </NavLink>
                 <NavLink to={"/player"}>
                     
-                    <a href="#_" class="inline-flex items-center justify-center w-full px-10 py-10 mb-2 text-xl text-white bg-black rounded-2xl sm:w-auto sm:mb-0">
+                    <a href="#_" class="inline-flex items-center justify-center w-full px-10 py-10 mb-2 text-xl text-white bg-gray-600 rounded-2xl sm:w-auto sm:mb-0">
                         Player
                     </a>
                     
--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -2,8 +2,37 @@ import "../components/login.css";
 import {useState} from "react";
 import { useNavigate } from "react-router-dom";
 function Login(){
-    return <div>
-        login
-    </div>
+    const navigate = useNavigate();
+
+    const handleLogin = () =>{
+      if(username&&password){
+        navigate("/")
+      }
+    }
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+  
+      return (
+          <div className="login">
+            <div className="loginWrapper">
+              <div className="loginLeft">
+                <h3 className="loginLogo">Recify</h3>
+                <span className="loginDesc">
+                 Explore your own music
+                </span>
+              </div>
+              <div className="loginRight">
+                <div className="loginBox">
+                  <input type = "text" placeholder="Username"   value={username} onChange={(event) => setUsername(event.target.value)}className="loginInput" />
+                  <input type = "password" placeholder= "Password" value={password} onChange={(event) => setPassword(event.target.value)} className="loginInput" />
+                  <button className="loginButton" onClick={()=> handleLogin()}>
+                    Sign In
+                    
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
 }
 export default Login;
