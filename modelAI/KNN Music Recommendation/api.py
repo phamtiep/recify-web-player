70fd43050de0aaa63f0cb8c8fbfb67357b8f46f6
VietAnh-AI2-UET <144088322+VietAnh-AI2-UET@users.noreply.github.com>
2024-05-16 14:19:03 +0700

    api return list of 10 recommended song
    api trả về 10 bài nhạc đề xuất

---
+++ modelAI/KNN Music Recommendation/api.py
@@ -0,0 +1,22 @@
+import requests
+import access_Database as acsdb
+from flask import Flask
+from flask_restful import Resource, Api
+from knn_recommender import Recommender
+
+app = Flask(__name__)
+api = Api(app)
+
+#xu li du lieu
+class Process(Resource):
+    def get(self, music_id):
+        model = Recommender(metric='cosine', algorithm='brute', data=acsdb.viewcnt_data, table=acsdb.viewcnt_data)
+        recommend_list = model.make_recommendation(song_id = music_id, n_recommendations=10)
+        return recommend_list
+
+api.add_resource(Process, '/recommend/<int:music_id>')
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+
