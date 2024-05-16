feff536ab6cecd7f10ba2801b3ce7fb1a4fcb3eb
VietAnh-AI2-UET <144088322+VietAnh-AI2-UET@users.noreply.github.com>
2024-05-16 18:37:06 +0700

    Update api.py

--- modelAI/KNN Music Recommendation/api.py
+++ modelAI/KNN Music Recommendation/api.py
@@ -8,12 +8,14 @@ app = Flask(__name__)
 api = Api(app)
 
 #xu li du lieu
+#return recommend_list la mot list int
 class Process(Resource):
     def get(self, music_id):
         model = Recommender(metric='cosine', algorithm='brute', data=acsdb.viewcnt_data, table=acsdb.viewcnt_table)
         recommend_list = model.make_recommendation(song_id = music_id, n_recommendations=10)
         return recommend_list
 
+#nhan id bai nhac dang nghe
 api.add_resource(Process, '/recommend/<int:music_id>')
 
 if __name__ == '__main__':
